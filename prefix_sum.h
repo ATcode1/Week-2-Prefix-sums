1c37e27c4a3a1e5182e00a7a03013b9a79afdad7
Adrian Torres <60658017+ATcode1@users.noreply.github.com>
2023-02-03 17:31:58 -0500

    Update prefix_sum.h

--- prefix_sum.h
+++ prefix_sum.h
@@ -1,37 +1,8 @@
-#ifndef __PREFIX_SUM_H
-#define __PREFIX_SUM_H
+#ifndef __PREFIXSUM_H
+#define __PREFIXSUM_H
 
-
-int prefix_sum(int array[], int length) {
-       int sum = 0;
-       for (int i=0; i < length; i++){
-               sum += array[i];
-        }
-       return sum;
-}
-
-bool non_negative_prefix_sum(int array[], int length) {
-        bool result = true;
-        int sum = 0;
-        for (int i = 0; i < length; i++) {
-                sum += array[i];
-                if (sum <= -1) {
-                        result = false;
-                }
-        }
-        return result;
-}
-
-bool non_positive_prefix_sum(int array[], int length) {
-        bool result = true;
-        int sum = 0;
-        for (int i = 0; i < length; i++) {
-                sum += array[i];
-                if (sum >= 1) {
-                        result = false;
-                }
-        }
-        return result;
-}
+int prefix_sum(int array[], int length);
+bool non_negative_prefix_sum(int array[], int length);
+bool non_positive_prefix_sum(int array[], int length);
 
 #endif
