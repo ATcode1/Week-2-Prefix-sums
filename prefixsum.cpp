ae6d626acdb318f3838254e3f98a552c6797bbeb
Adrian Torres <60658017+ATcode1@users.noreply.github.com>
2023-02-03 17:33:46 -0500

    Create prefixsum.cpp

---
+++ prefixsum.cpp
@@ -0,0 +1,34 @@
+#include "prefixsum.h"
+#include <iostream>
+
+int prefix_sum(int array[], int length) {
+       int sum = 0;
+       for (int i=0; i < length; i++){
+               sum += array[i];
+        }
+       return sum;
+}
+
+bool non_negative_prefix_sum(int array[], int length) {
+        bool result = true;
+        int sum = 0;
+        for (int i = 0; i < length; i++) {
+                sum += array[i];
+                if (sum <= -1) {
+                        result = false;
+                }
+        }
+        return result;
+}
+
+bool non_positive_prefix_sum(int array[], int length) {
+        bool result = true;
+        int sum = 0;
+        for (int i = 0; i < length; i++) {
+                sum += array[i];
+                if (sum >= 1) {
+                        result = false;
+                }
+        }
+        return result;
+}
